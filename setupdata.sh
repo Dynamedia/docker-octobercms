ee7cacc8eb36826a2e9e918942fce5a2dfb2cf78
Rob Ballantyne <rob@dynamedia.uk>
2018-12-07 16:30:25 +0000

    add volume mount points as defined in the given docker-compose.yml file

---
+++ setupdata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ./data/{storage/{media,uploads/public},plugins,themes}
+touch ./data/storage/media/.gitkeep
+touch ./data/storage/uploads/public/.gitkeep
+touch ./data/plugins/.gitkeep
+touch ./data/themes/.gitkeep

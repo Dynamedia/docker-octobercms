adca44455b8f46c63e360702f6c6ef2eaf5f653c
Rob Ballantyne <rob@dynamedia.uk>
2021-09-01 11:11:37 +0100

    notes

--- entrypoint-mysql.sh
+++ entrypoint-mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "extracting build time databases..."
-# -k option will not clobber existing files, so its safe to restart with new data in a mount/volume
+# -k option will not clobber existing files, so its safe to restart with newer data in a mount/volume
 tar -k -zxf /var/lib/mysql-overlay.tar.gz -C /var/lib/ > /dev/null 2>&1
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
